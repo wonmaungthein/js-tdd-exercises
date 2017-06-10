694b410b23e53b7a368bd29e5828d9492164f916
Won Maung Thein <wonmaungthein@gmail.com>
2017-06-10 14:22:01 +0100

    Finished implementing and testing find alive persons

--- 01-exercises/find-alive-people.test.js
+++ 01-exercises/find-alive-people.test.js
@@ -1,10 +1,21 @@
 function findAlive(writers) {
+  var array = [];
+  if (writers === undefined) {
+    return [];
+  }
+  for (var i = 0; i < writers.length; i++) {
+    if (writers[i].alive === true) {
+      array.push(writers[i]);
+    }
+  }
+  return array;
+
   // ToDo: Implement this function to make all the tests below pass
   // DO NOT Change the tests (you can add more tests though)
 }
 
 
-test('function should retrieve only alive writers', function() {
+test('function should retrieve only alive writers', function () {
   var persons = [
     {
       firstName: "Virginia",
@@ -39,17 +50,82 @@ test('function should retrieve only alive writers', function() {
   var result = findAlive(persons);
 
   expect(result).toEqual([{
+    firstName: "Zadie",
+    lastName: "Smith",
+    occupation: "writer",
+    age: 41,
+    alive: true
+  },
+  {
+    firstName: "bell",
+    lastName: "hooks",
+    occupation: "writer",
+    age: 64,
+    alive: true
+  }])
+});
+
+
+test('function should return an empty list if there are no alive writers', function () {
+  var persons = [
+    {
+      firstName: "Virginia",
+      lastName: "Woolf",
+      occupation: "writer",
+      age: 59,
+      alive: false
+    },
+    {
       firstName: "Zadie",
       lastName: "Smith",
       occupation: "writer",
       age: 41,
-      alive: true
+      alive: false
+    },
+    {
+      firstName: "Jane",
+      lastName: "Austen",
+      occupation: "writer",
+      age: 41,
+      alive: false
     },
     {
       firstName: "bell",
       lastName: "hooks",
       occupation: "writer",
       age: 64,
-      alive: true
-    }])
+      alive: false
+    },
+  ];
+
+  var result = findAlive(persons);
+  expect(result).toEqual([]);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+test('function should return an empty list if the list of writers are in empty array', function () {
+  var persons = [
+
+  ];
+
+  var result = findAlive(persons);
+  expect(result).toEqual([]);
+});
+
+test('function should not throw error if writers list is not defined', function () {
+  expect(findAlive).not.toThrow();
+  expect(findAlive()).toEqual([]);
+});
+
